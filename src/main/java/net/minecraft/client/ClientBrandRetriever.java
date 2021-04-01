2aec0c1893faba9920c1e47b3b96437fe50a7a5e
Nicholas Blackburn <nickblackburn02@gmail.com>
2021-04-01 14:43:58 -0400

    added new clinet brand to mc

--- src/main/java/net/minecraft/client/ClientBrandRetriever.java
+++ src/main/java/net/minecraft/client/ClientBrandRetriever.java
@@ -6,6 +6,6 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 @OnlyIn(Dist.CLIENT)
 public class ClientBrandRetriever {
    public static String getClientModName() {
-      return "vanilla";
+      return "Blackburn-Hacked";
    }
 }
