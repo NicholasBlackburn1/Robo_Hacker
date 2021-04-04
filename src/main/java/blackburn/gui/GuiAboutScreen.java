caf57ee946e0eab1008858cd096879d3e71d8b0c
Nicholas Blackburn <nickblackburn02@gmail.com>
2021-04-04 10:06:06 -0400

    added image to background of about page but it does no want to render over text need to fix it

--- src/main/java/blackburn/gui/GuiAboutScreen.java
+++ src/main/java/blackburn/gui/GuiAboutScreen.java
@@ -1,11 +1,14 @@
 package blackburn.gui;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
 
 import net.minecraft.client.GameSettings;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.Widget;
 import net.minecraft.client.resources.I18n;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.StringTextComponent;
 import net.optifine.gui.GuiButtonOF;
 import net.optifine.gui.GuiScreenOF;
@@ -16,6 +19,7 @@ public class GuiAboutScreen extends GuiScreenOF
 {
     private Screen prevScreen;
     private GameSettings settings;
+    private String splasher;
     
    // private static AbstractOption[] enumOptions = new AbstractOption[] {AbstractOption.CLOUDS, AbstractOption.CLOUD_HEIGHT, AbstractOption.TREES, AbstractOption.RAIN, AbstractOption.SKY, AbstractOption.STARS, AbstractOption.SUN_MOON, AbstractOption.SHOW_CAPES, AbstractOption.FOG_FANCY, AbstractOption.FOG_START, AbstractOption.TRANSLUCENT_BLOCKS, AbstractOption.HELD_ITEM_TOOLTIPS, AbstractOption.DROPPED_ITEMS, AbstractOption.SWAMP_COLORS, AbstractOption.VIGNETTE, AbstractOption.ALTERNATE_BLOCKS, AbstractOption.ENTITY_DISTANCE_SCALING, AbstractOption.BIOME_BLEND_RADIUS};
     private TooltipManager tooltipManager = new TooltipManager(this, new TooltipProviderOptions());
@@ -29,7 +33,7 @@ public class GuiAboutScreen extends GuiScreenOF
 
     public void init()
     {
-    
+      
         this.addButton(new GuiButtonOF(200, this.width / 2 - 100, this.height / 6 + 200 + 11, I18n.format("gui.done")));
     }
 
@@ -58,7 +62,11 @@ public class GuiAboutScreen extends GuiScreenOF
 
     public void render(MatrixStack matrixStackIn, int x, int y, float partialTicks)
     {
-        this.renderBackground(matrixStackIn);
+        //this.renderBackground(matrixStackIn);
+        this.minecraft.getTextureManager().bindTexture(new ResourceLocation("textures/gui/title/background/background.png"));
+        RenderSystem.enableBlend();
+        RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+     
         drawCenteredString(matrixStackIn, this.minecraft.fontRenderer, this.title, this.width / 2, 15, 16777215);
 
         this.drawString(matrixStackIn, this.font, "UwU TesT", 2, this.height - 10, 16777215 );
