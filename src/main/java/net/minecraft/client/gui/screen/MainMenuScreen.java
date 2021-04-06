12aec2edd008c969dac9a0803b1cccb9f615ca00
Nicholas Blackburn <nickblackburn02@gmail.com>
2021-04-06 07:28:52 -0400

    fixed Background Image crashing

--- src/main/java/net/minecraft/client/gui/screen/MainMenuScreen.java
+++ src/main/java/net/minecraft/client/gui/screen/MainMenuScreen.java
@@ -206,7 +206,7 @@ public class MainMenuScreen extends Screen
         //this.minecraft.getTextureManager().bindTexture(new ResourceLocation("textures/gui/title/background/1413249501665.png"));
         
         //Adding Custon Image Placement into Client 
-        this.minecraft.getTextureManager().bindTexture(new ResourceLocation(MainMenuConfig.updateMainMenuBackground()));
+        this.minecraft.getTextureManager().bindTexture(new ResourceLocation("textures/gui/title/background/background.png"));
         RenderSystem.enableBlend();
         RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
         RenderSystem.color4f(1.0F, 1.0F, 1.0F, this.showFadeInAnimation ? (float)MathHelper.ceil(MathHelper.clamp(f, 0.0F, 1.0F)) : 1.0F);
@@ -214,7 +214,7 @@ public class MainMenuScreen extends Screen
         float f1 = this.showFadeInAnimation ? MathHelper.clamp(f - 1.0F, 0.0F, 1.0F) : 1.0F;
         int l = MathHelper.ceil(f1 * 255.0F) << 24;
         if ((l & -67108864) != 0) {
-           this.minecraft.getTextureManager().bindTexture(new ResourceLocation(MainMenuConfig.updateMainMenuBackground()));
+           this.minecraft.getTextureManager().bindTexture(new ResourceLocation("textures/gui/title/background/background.png"));
            RenderSystem.color4f(1.0F, 1.0F, 1.0F, f1);
            if (this.showTitleWronglySpelled) {
               
